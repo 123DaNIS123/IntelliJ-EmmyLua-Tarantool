e488e1f7d75a456b1e62cbfb2157266209ddbf6f
123DaNIS123 <danisnizamutdinov1@gmail.com>
2022-08-05 19:22:56 +0300

    made till tap module

--- src/main/resources/std/Lua50/string.lua
+++ src/main/resources/std/Lua50/string.lua
@@ -231,3 +231,86 @@ function string.sub(s, i, j) end
 ---@param s string
 ---@return string
 function string.upper(s) end
+
+--
+
+---
+--- Return the string left-justified in a string of length width.
+--- @param input_string string @ the string to left-justify
+--- @param width integer @ the width of the string after left-justifying
+--- @param pad_character string @ a single character, default = 1 space
+--- @return string @ left-justified string (unchanged if width <= string length)
+function string.ljust(input_string, width, pad_character) end
+
+---
+--- Return the string right-justified in a string of length width.
+--- @param input_string string @ the string to right-justify
+--- @param width integer @ the width of the string after right-justifying
+--- @param pad_character string @ a single character, default = 1 space
+--- @return string @ right-justified string (unchanged if width <= string length)
+function string.rjust(input_string, width, pad_character) end
+
+---
+--- Return the hexadecimal value of the input string.
+--- @param input_string string @ the string to process
+--- @return string @ hexadecimal, 2 hex-digit characters for each input character
+function string.hex(input_string) end
+
+---
+--- Given a string containing pairs of hexadecimal digits, return a string with one byte for each pair. This is
+--- the reverse of string.hex(). The hexadecimal-input-string must contain an even number of hexadecimal digits.
+--- @param hexadecimal_input_string string @ string with pairs of hexadecimal digits
+--- @return string @ string with one byte for each pair of hexadecimal digits
+function string.fromhex(hexadecimal_input_string) end
+
+---
+--- Return True if input-string starts with start-string, otherwise return False.
+--- @param input_string string @ the string where start-string should be looked for
+--- @param start_string string @ the string to look for
+--- @param start_pos integer @ position: where to start looking within input-string
+--- @param end_pos integer @ position: where to end looking within input-string
+--- @return boolean @ true or false
+function string.startswith(input_string, start_string, start_pos, end_pos) end
+
+---
+--- Return True if input-string ends with end-string, otherwise return False.
+--- @param input_string string @  the string where end-string should be looked for
+--- @param start_string string @ the string to look for
+--- @param start_pos integer @ position: where to start looking within input-string
+--- @param end_pos integer @ position: where to end looking within input-string
+--- @return boolean @ true or false
+function string.startswith(input_string, start_string, start_pos, end_pos) end
+
+---
+--- Return the value of the input string, after removing characters on the left. The optional list-of-characters
+--- parameter is a set not a sequence, so string.lstrip(...,'ABC') does not mean strip 'ABC', it means strip 'A' or 'B' or 'C'.
+--- @param input_string string @ the string to process
+--- @param list_of_characters string @ what characters can be stripped. Default = space.
+--- @return string @ result after stripping characters from input string
+function string.lstrip(input_string, list_of_characters) end
+
+---
+--- Return the value of the input string, after removing characters on the right. The optional
+--- list-of-characters parameter is a set not a sequence, so string.rstrip(...,'ABC') does not mean strip 'ABC',
+--- it means strip 'A' or 'B' or 'C'.
+--- @param input_string string @ the string to process
+--- @param list_of_characters string @ what characters can be stripped. Default = space.
+--- @return string @ result after stripping characters from input string
+function string.rstrip(input_string, list_of_characters) end
+
+---
+--- Split input-string into one or more output strings in a table. The places to split are the places where split-string occurs.
+--- @param input_string string @ the string to split
+--- @param split_string integer @ the string to find within input-string. Default = space.
+--- @param max integer @ maximum number of delimiters to process counting from the beginning of the input string. Result will contain max + 1 parts maximum.
+--- @return table @ table of strings that were split from input-string
+function string.split(input_string, split_string, max) end
+
+---
+--- Return the value of the input string, after removing characters on the left and the right. The optional
+--- list-of-characters parameter is a set not a sequence, so string.strip(...,'ABC') does not mean strip 'ABC',
+--- it means strip 'A' or 'B' or 'C'.
+--- @param input_string string @ the string to process
+--- @param list_of_characters string @ what characters can be stripped. Default = space.
+--- @return string @ result after stripping characters from input string
+function string.strip(input_string, list_of_characters) end
---
+++ src/main/resources/std/Lua50/tap.lua
@@ -0,0 +1,137 @@
+---
+--- The tap module streamlines the testing of other modules. It allows writing of tests in the TAP protocol. The
+--- results from the tests can be parsed by standard TAP-analyzers so they can be passed to utilities such as prove.
+--- Thus one can run tests and then use the results for statistics, decision-making, and so on.
+--- @module tap
+
+---
+--- Initialize.
+---
+--- The result of tap.test is an object, which will be called taptest in the rest of this discussion,
+--- which is necessary for taptest:plan() and all the other methods.
+--- @param test_name table @ taptest
+function tap.test(test_name) end
+
+---
+--- @class Taptest
+local taptest = {}
+
+---
+--- Create a subtest (if no func argument specified), or (if all arguments are specified) create a subtest,
+--- run the test function and print the result.
+--- @param test_name string @ an arbitrary name to give for the test outputs.
+--- @param func function @ the test logic to run.
+--- @return userdata|string @ taptest
+function taptest:test(test_name, func)) end
+
+---
+--- Indicate how many tests will be performed.
+--- @param count number
+--- @return nil
+function taptest:plan(count) end
+
+---
+--- Checks the number of tests performed.
+---
+--- The result will be a display saying # bad plan: ... if the number of completed tests is not equal to the number of
+--- tests specified by taptest:plan(...). (This is a purely Tarantool feature: “bad plan” messages are out of the TAP13 standard.)
+---
+--- This check should only be done after all planned tests are complete, so ordinarily taptest:check() will only appear
+--- at the end of a script. However, as a Tarantool extension, taptest:check() may appear at the end of any subtest.
+--- Therefore there are three ways to cause the check:
+---
+--- by calling taptest:check() at the end of a script,
+--- by calling a function which ends with a call to taptest:check(),
+--- or by calling taptest:test(‘…’, subtest-function-name) where subtest-function-name does not need to end with
+--- taptest:check() because it can be called after the subtest is complete.
+--- @return boolean @ true or false
+function taptest:check() end
+
+---
+--- Display a diagnostic message.
+--- @param message string @ the message to be displayed.
+--- @return nil
+function taptest:diag(message) end
+
+---
+--- Display a diagnostic message.
+--- @param condition boolean @ an expression which is true or false
+--- @param test_name string @ name of the test
+--- @return boolean @ true or false
+function taptest:ok(condition, test_name) end
+
+---
+--- taptest:fail('x') is equivalent to taptest:ok(false, 'x'). Displays the message.
+--- @param test_name string @ name of the test
+--- @return boolean @ true or false
+function taptest:fail(test_name) end
+
+---
+--- taptest:skip('x') is equivalent to taptest:ok(true, 'x' .. '# skip'). Displays the message.
+--- @param test_name string @ name of the test
+--- @return nil
+function taptest:skip(test_name) end
+
+---
+--- Check whether the first argument equals the second argument. Displays extensive message if the result is false.
+--- @param got number @ actual result
+--- @param expected number @ expected result
+--- @param test_name string @ name of the test
+--- @return boolean @ true or false.
+function taptest:is(got, expected, test_name) end
+
+---
+--- This is the negation of taptest:is().
+--- @param got number @ actual result
+--- @param expected number @ expected result
+--- @param test_name string @ name of the test
+--- @return boolean @ true or false.
+function taptest:isnt(got, expected, test_name) end
+
+---
+--- Recursive version of taptest:is(...), which can be be used to compare tables as well as scalar values.
+--- @param got number @ actual result
+--- @param expected number @ expected result
+--- @param test_name string @ name of the test
+--- @return boolean @ true or false.
+function taptest:is_deeply(got, expected, test_name) end
+
+---
+--- Verify a string against a pattern. Ok if match is found.
+--- @param got number @ actual result
+--- @param expected number @ pattern
+--- @param test_name string @ name of the test
+--- @return boolean @ true or false.
+function taptest:like(got, expected, test_name) end
+
+---
+--- This is the negation of taptest:like().
+--- @param got number @ actual result
+--- @param expected number @ pattern
+--- @param test_name string @ name of the test
+--- @return boolean @ true or false.
+function taptest:unlike(got, expected, test_name) end
+
+---
+function taptest:isnil(balue, message, extra) end
+
+---
+function taptest:isstring(balue, message, extra) end
+
+---
+function taptest:isnumber(balue, message, extra) end
+
+---
+function taptest:istable(balue, message, extra) end
+
+---
+function taptest:isboolean(balue, message, extra) end
+
+---
+function taptest:isudata(balue, message, extra) end
+
+---
+--- Test whether a value has a particular type. Displays a long message if the value is not of the specified type.
+--- @param value lua_value @ value the type of which is to be checked
+--- @param utype string @ type of data that a passed value should have
+function taptest:iscdata(balue, message, extra) end
