9311294dcc9b2be0bde3cc9c125c9a75aad3e3c3
123DaNIS123 <danisnizamutdinov1@gmail.com>
2022-08-06 10:32:11 +0300

    only Lua51

--- src/main/java/com/tarantoollua/intellij/lua/codeInsight/inspection/LanguageLevelInspection.kt
+++ src/main/java/com/tarantoollua/intellij/lua/codeInsight/inspection/LanguageLevelInspection.kt
@@ -45,14 +45,14 @@ class LanguageLevelInspection : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : LuaVisitor() {
             override fun visitBinaryOp(o: LuaBinaryOp) {
-                if (o.languageLevel < LuaLanguageLevel.LUA51 && LuaParserDefinition.LUA52_BIN_OP_SET.contains(o.node.firstChildNode.elementType)) {
+                if (o.languageLevel < LuaLanguageLevel.LUA52 && LuaParserDefinition.LUA52_BIN_OP_SET.contains(o.node.firstChildNode.elementType)) {
                     val desc = "The binary operator '${o.text}' only available in Lua 5.2 or above"
                     registerOperatorProblem(o, desc, holder)
                 }
             }
 
             override fun visitUnaryOp(o: LuaUnaryOp) {
-                if (o.languageLevel < LuaLanguageLevel.LUA51 && LuaParserDefinition.LUA52_UNARY_OP_SET.contains(o.node.firstChildNode.elementType)) {
+                if (o.languageLevel < LuaLanguageLevel.LUA52 && LuaParserDefinition.LUA52_UNARY_OP_SET.contains(o.node.firstChildNode.elementType)) {
                     val desc = "The unary operator '${o.text}' only available in Lua 5.2 or above"
                     registerOperatorProblem(o, desc, holder)
                 }
